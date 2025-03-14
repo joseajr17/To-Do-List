bb6df20db8ef045af9b558e41ecba95409bfa846
José Antonio <josejr017@gmail.com>
2025-03-14 14:26:48 -0300

    server update

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-import express from 'express';
+const express = require('express')
 const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
 const app = express();
+app.use(express.json());
 
 // CRUD
 
@@ -11,33 +12,50 @@ const app = express();
 app.post('/tasks', async (req, res) => {
     let task = await prisma.task.create({
         data: {
-            text: req.body.text
+            text: req.body.text,
         },
     });
-    res.json(task);
+    res.status(201).json(task);
 })
 
 // Listar tasks
-app.get('/task', async (req, res) => {
+app.get('/tasks', async (req, res) => {
     let tasks = await prisma.task.findMany();
-    res.json(tasks);
+    res.status(200).json(tasks);
 });
 
-// Atualizar tarefa p/ concluída
-app.patch('/task:id', async (req, res) => {
-    const task = await prisma.task.update({
-        where: {
-            id: req.params.id
-        },
-        data: {
-            completed: true
-        },
-    });
-    res.json(task);
+// Atualizar o estado da tarefa
+app.patch('/tasks/:id', async (req, res) => {
+    try {
+        // Busca a tarefa pelo ID
+        const task = await prisma.task.findUnique({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if (!task) {
+            return res.status(404).json({ error: "Tarefa não encontrada" });
+        }
+
+        const updatedTask = await prisma.task.update({
+            where: {
+                id: req.params.id
+            },
+            data: {
+                completed: !task.completed
+            },
+        });
+
+        res.json(updatedTask);
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao atualizar a tarefa" });
+    }
 });
+
 
 // Deletar Task
-app.delete('/task/:id', async (req, res) => {
+app.delete('/tasks/:id', async (req, res) => {
     await prisma.task.delete({
         where: {
             id: req.params.id
