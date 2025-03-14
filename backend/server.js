b8b30218d64b32e91211eb07d7b76fe0a58538eb
José Antonio <josejr017@gmail.com>
2025-03-14 12:57:34 -0300

    Update task to completed

--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,17 @@ app.get('/task', async (req, res) => {
     res.json(tasks);
 });
 
+// Atualizar tarefa p/ concluída
+app.patch('/task:id', async (req, res) => {
+    const task = await prisma.task.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            completed: true
+        },
+    });
+    res.json(task);
+});
+
 app.listen(5000, () => console.log("Server rodando"));
