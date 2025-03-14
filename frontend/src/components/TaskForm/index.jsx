6d7c18942359059e5535e814dca0b5332da07f04
José Antonio <josejr017@gmail.com>
2025-03-14 20:14:01 -0300

    <TaskForm />

---
+++ frontend/src/components/TaskForm/index.jsx
@@ -0,0 +1,14 @@
+import plus from '../../assets/plus.png';
+
+export function TaskForm() {
+    return (
+        <form className="flex items-center justify-between bg-gray-700 m-1 rounded-lg ring ring-gray-900/5 mb-5 p-2">
+            <button type="submit" className=" bg-white hover:bg-sky-700 cursor-pointer px-2 py-2 rounded-full  flex items-center justify-center w-12 h-12">
+            <img src={plus} alt="+" class="w-6 h-6"></img>
+            </button>
+            <input placeholder="Adicionar uma nova tarefa" className="rounded ml-2 w-full"/>
+            
+        </form>
+
+    );
+}
--- frontend/src/components/TaskList/index.jsx
+++ frontend/src/components/TaskList/index.jsx
@@ -1,7 +1,10 @@
 import { Task } from "../Task";
+import { TaskForm } from "../TaskForm";
+
 export function TaskList() {
     return (
         <div className="flex flex-col m-2 text-white pt-4">
+                <TaskForm />
                 <Task />
                 <Task />
         </div>
